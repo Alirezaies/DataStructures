06996a5bde70e72ef4340071637e1f1b1df9b79f
AlirezaieS <sadegh@webgo.ir>
2017-06-30 21:19:37 +0430

    Stack

---
+++ Stack/include/LinkedList.h
@@ -0,0 +1,32 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef int type;
+
+struct node{
+    type Data;
+    int ID; //Help Us For Searching By Index;
+    node *next;
+};
+
+class linkedlist{
+    public:
+        linkedlist();
+        ~linkedlist();
+        void AddFirst(type info);
+        void Append(type info);
+        void Traverse();
+        void SearchByID(int ID);
+        void SearchByData(type info);
+        void DeleteByIndex(int index);
+        void SortID();
+        void insert(type record, int index);
+        bool isEmpty();
+        int getMsize();
+
+    private:
+        node *first;
+        node *last;
+        int mSize;
+};
+#endif
